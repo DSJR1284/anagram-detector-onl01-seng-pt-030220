60f6393d91d1fe013f8aed2fa313734fb37df277
Doug Jones <dsjr1284@gmail.com>
2020-04-01 22:54:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram 
+
+ attr_accessor :name 
+
+ new_array=[]
+
+ def initialize(name)
+   @name=name  
+
+ end 
+
+  def match(array) 
+  
+  
+  end 
+  
+
+
+
+
+
+end 
